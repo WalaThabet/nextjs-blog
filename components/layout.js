d55b8f38cc4e706b1156bf84235840e0222a25b1
Wala Thabet <walathab@gmail.com>
2022-04-25 17:40:42 +0200

    changing title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Wala Thabet'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Badass Bitch Blog'
 
 export default function Layout({ children, home }) {
   return (
